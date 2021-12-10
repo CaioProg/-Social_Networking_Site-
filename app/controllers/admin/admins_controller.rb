6e9cc8705438b43c0e42aa4b8c623478c48bc9b3
Caio Lucas <caioprog18@gmail.com>
2021-12-10 11:25:21 -0300

    Creating new adm and edit adm

--- app/controllers/admin/admins_controller.rb
+++ app/controllers/admin/admins_controller.rb
@@ -1,4 +1,5 @@
 class Admin::AdminsController < AdminController
+  before_action :set_admin, only: [:edit, :update, :destrozy]
 
   def index
     @admins = Admin.order(id: :desc)
@@ -7,11 +8,45 @@ class Admin::AdminsController < AdminController
   def new
     @admin = Admin.new
   end
-  
-  private 
+
+  def create
+    @admin = Admin.new(form_params)
+
+    if @admin.save
+      redirect_to admin_admins_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @admin = Admin.find(params[:id])
+  end
+
+  def update
+    params = form_params.to_h
+    params = params.except!(:password, :password_confirmation) if params[:password].blank?
+
+    if @admin.update(params)
+      redirect_to admin_admins_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @admin.destroy
+    redirect_to admin_admins_path
+  end
+
+  private
 
   def form_params
-    params.require(:admin).permit(:name, :email, :password_confirmation)
+    params.require(:admin).permit(:name, :email, :password, :password_confirmation)
+  end
+
+  def set_admin
+    @admin = Admin.find(params[:id])
   end
 
 end
